a1797b4af8320abcb24b383daa8e4948d623c017
u66u <cake3327@gmail.com>
2024-12-29 16:57:38 +0300

    need to fix while

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,6 @@ enum Token {
     Less,
     Equal,
     NotEqual,
-    Print
-
 }
 
 #[derive(Debug)]
@@ -33,10 +31,9 @@ enum Instruction {
     Less,
     Equal,
     NotEqual,
-    Jmp(usize),      
-    Jz(usize),       
-    Label(String),    
-    Print
+    Jmp(usize),
+    Jz(usize),
+    Label(String),
 }
 
 #[derive(Debug, Clone)]
@@ -60,27 +57,19 @@ enum ASTNode {
 
 struct VM {
     stack: Vec<i32>,
-    ip: usize, 
+    ip: usize,
 }
 
 impl VM {
     fn new() -> Self {
-        VM { stack: Vec::new(), ip: 0 }
+        VM {
+            stack: Vec::new(),
+            ip: 0,
+        }
     }
 
     fn execute(&mut self, instructions: &[Instruction]) {
-        let mut real_positions: Vec<usize> = Vec::new();
-        let mut real_pos = 0;
-        
-        for (pos, instruction) in instructions.iter().enumerate() {
-            real_positions.push(real_pos);
-            if !matches!(instruction, Instruction::Label(_)) {
-                real_pos += 1;
-            }
-        }
-
         while self.ip < instructions.len() {
-            println!("IP: {}, Stack: {:?}", self.ip, self.stack);
             match &instructions[self.ip] {
                 Instruction::Push(value) => self.stack.push(*value),
                 Instruction::Pop => {
@@ -127,31 +116,17 @@ impl VM {
                     self.stack.push(if a != b { 1 } else { 0 });
                 }
                 Instruction::Jmp(target) => {
-                    
-                    self.ip = real_positions.iter()
-                        .position(|&x| x == *target)
-                        .expect("Invalid jump target");
+                    self.ip = *target;
                     continue;
                 }
                 Instruction::Jz(target) => {
                     let value = self.stack.pop().expect("Stack underflow");
                     if value == 0 {
-                        self.ip = real_positions.iter()
-                            .position(|&x| x == *target)
-                            .expect("Invalid jump target");
+                        self.ip = *target;
                         continue;
                     }
                 }
-
-                Instruction::Print => {
-                    let value = self.stack.last().expect("Stack underflow");
-                    println!("Output: {}", value);
-                }
-                Instruction::Label(_) => {
-                    self.ip += 1;
-                    continue;
-                }
-
+                Instruction::Label(_) => {}
             }
             self.ip += 1;
         }
@@ -169,79 +144,79 @@ impl Tokenizer {
         Tokenizer { input, position: 0 }
     }
 
-        fn next_token(&mut self) -> Token {
-            while self.position < self.input.len() {
-                let c = self.input.chars().nth(self.position).unwrap();
-                match c {
-                    '0'..='9' => {
-                        let mut num = 0;
-                        while self.position < self.input.len() {
-                            let c = self.input.chars().nth(self.position).unwrap();
-                            if !c.is_ascii_digit() {
-                                break;
-                            }
-                            num = num * 10 + (c as i32 - '0' as i32);
-                            self.position += 1;
+    fn next_token(&mut self) -> Token {
+        while self.position < self.input.len() {
+            let c = self.input.chars().nth(self.position).unwrap();
+            match c {
+                '0'..='9' => {
+                    let mut num = 0;
+                    while self.position < self.input.len() {
+                        let c = self.input.chars().nth(self.position).unwrap();
+                        if !c.is_ascii_digit() {
+                            break;
                         }
-                        return Token::Number(num);
-                    }
-                    '+' => {
+                        num = num * 10 + (c as i32 - '0' as i32);
                         self.position += 1;
-                        return Token::Plus;
                     }
-                    '-' => {
-                        self.position += 1;
-                        return Token::Minus;
+                    return Token::Number(num);
+                }
+                '+' => {
+                    self.position += 1;
+                    return Token::Plus;
+                }
+                '-' => {
+                    self.position += 1;
+                    return Token::Minus;
+                }
+                '*' => {
+                    self.position += 1;
+                    return Token::Star;
+                }
+                '/' => {
+                    self.position += 1;
+                    return Token::Slash;
+                }
+                '(' => {
+                    self.position += 1;
+                    return Token::LParen;
+                }
+                ')' => {
+                    self.position += 1;
+                    return Token::RParen;
+                }
+                '{' => {
+                    self.position += 1;
+                    return Token::LBrace;
+                }
+                '}' => {
+                    self.position += 1;
+                    return Token::RBrace;
+                }
+                '>' => {
+                    self.position += 1;
+                    return Token::Greater;
+                }
+                '<' => {
+                    self.position += 1;
+                    return Token::Less;
+                }
+                '=' => {
+                    if self.input[self.position..].starts_with("==") {
+                        self.position += 2;
+                        return Token::Equal;
+                    } else {
+                        panic!("Unexpected token: =");
                     }
-                    '*' => {
-                        self.position += 1;
-                        return Token::Star;
+                }
+                '!' => {
+                    if self.input[self.position..].starts_with("!=") {
+                        self.position += 2;
+                        return Token::NotEqual;
+                    } else {
+                        panic!("Unexpected token: !");
                     }
-                    '/' => {
-                        self.position += 1;
-                        return Token::Slash;
-                    }
-                    '(' => {
-                        self.position += 1;
-                        return Token::LParen;
-                    }
-                    ')' => {
-                        self.position += 1;
-                        return Token::RParen;
-                    }
-                    '{' => {
-                        self.position += 1;
-                        return Token::LBrace;
-                    }
-                    '}' => {
-                        self.position += 1;
-                        return Token::RBrace;
-                    }
-                    '>' => {
-                        self.position += 1;
-                        return Token::Greater;
-                    }
-                    '<' => {
-                        self.position += 1;
-                        return Token::Less;
-                    }
-                    '=' => {
-                        if self.input[self.position..].starts_with("==") {
-                            self.position += 2;
-                            return Token::Equal;
-                        } else {
-                            panic!("Unexpected token: =");
-                        }
-                    }
-                    '!' => {
-                        if self.input[self.position..].starts_with("!=") {
-                            self.position += 2;
-                            return Token::NotEqual;
-                        } else {
-                            panic!("Unexpected token: !");
-                        }
-                    }
-                    'i' => {
+                }
+                'i' => {
                     if self.input[self.position..].starts_with("if") {
                         self.position += 2;
                         return Token::If;
@@ -259,26 +234,25 @@ impl Tokenizer {
                         return Token::Else;
                     }
                 }
-                    _ => {
-                        self.position += 1;
-                    }
+                _ => {
+                    self.position += 1;
                 }
             }
-            Token::EOF
         }
-        fn tokenize(&mut self) -> Vec<Token> {
-            let mut tokens = Vec::new();
-            loop {
-                let token = self.next_token();
-                tokens.push(token.clone());
-                if token == Token::EOF {
-                    break;
-                }
-            }
-            tokens
-        }
-
+        Token::EOF
     }
+    fn tokenize(&mut self) -> Vec<Token> {
+        let mut tokens = Vec::new();
+        loop {
+            let token = self.next_token();
+            tokens.push(token.clone());
+            if token == Token::EOF {
+                break;
+            }
+        }
+        tokens
+    }
+}
 
 struct Parser {
     tokenizer: Tokenizer,
@@ -306,7 +280,6 @@ impl Parser {
         statements
     }
 
-
     fn eat(&mut self, token: Token) {
         if self.current_token == token {
             self.current_token = self.tokenizer.next_token();
@@ -324,7 +297,7 @@ impl Parser {
             }
             Token::LParen => {
                 self.eat(Token::LParen);
-                let node = self.comparison(); 
+                let node = self.comparison();
                 self.eat(Token::RParen);
                 node
             }
@@ -372,7 +345,7 @@ impl Parser {
     }
 
     fn comparison(&mut self) -> ASTNode {
-        let mut node = self.expr(); 
+        let mut node = self.expr();
         while matches!(
             self.current_token,
             Token::Greater | Token::Less | Token::Equal | Token::NotEqual
@@ -388,23 +361,23 @@ impl Parser {
             node = ASTNode::BinOp {
                 left: Box::new(node),
                 op,
-                right: Box::new(self.expr()), 
+                right: Box::new(self.expr()),
             };
         }
         node
     }
 
     fn if_statement(&mut self) -> ASTNode {
-        self.eat(Token::If); 
-        self.eat(Token::LParen); 
-        let condition = self.comparison(); 
-        self.eat(Token::RParen); 
-        let if_block = self.block(); 
+        self.eat(Token::If);
+        self.eat(Token::LParen);
+        let condition = self.comparison();
+        self.eat(Token::RParen);
+        let if_block = self.block();
         let else_block = if self.current_token == Token::Else {
-            self.eat(Token::Else); 
-            self.block() 
+            self.eat(Token::Else);
+            self.block()
         } else {
-            Vec::new() 
+            Vec::new()
         };
         ASTNode::If {
             condition: Box::new(condition),
@@ -414,11 +387,11 @@ impl Parser {
     }
 
     fn while_loop(&mut self) -> ASTNode {
-        self.eat(Token::While); 
-        self.eat(Token::LParen); 
-        let condition = self.comparison(); 
-        self.eat(Token::RParen); 
-        let body = self.block(); 
+        self.eat(Token::While);
+        self.eat(Token::LParen);
+        let condition = self.comparison();
+        self.eat(Token::RParen);
+        let body = self.block();
         ASTNode::While {
             condition: Box::new(condition),
             body,
@@ -426,17 +399,15 @@ impl Parser {
     }
 
     fn block(&mut self) -> Vec<ASTNode> {
-        self.eat(Token::LBrace); 
+        self.eat(Token::LBrace);
         let mut nodes = Vec::new();
         while self.current_token != Token::RBrace {
-            nodes.push(self.expr()); 
+            nodes.push(self.expr());
         }
-        self.eat(Token::RBrace); 
+        self.eat(Token::RBrace);
         nodes
     }
 }
-
-
 
 fn compile(node: ASTNode) -> Vec<Instruction> {
     match node {
@@ -457,105 +428,43 @@ fn compile(node: ASTNode) -> Vec<Instruction> {
             }
             instructions
         }
-
-        ASTNode::If { condition, if_block, else_block } => {
-            let mut instructions = Vec::new();
-            
-            
-            instructions.push(Instruction::Label("if_start".to_string()));
-            
-            
-            instructions.extend(compile(*condition));
-            
-            
-            let jz_pos = instructions.len();
-            instructions.push(Instruction::Jz(0));  
-            
-            
-            let if_block_start = instructions.len();
-            for stmt in if_block {
-                instructions.extend(compile(stmt));
-            }
-            
-            
-            let jmp_pos = instructions.len();
-            instructions.push(Instruction::Jmp(0));  
-            
-            
-            let else_block_start = instructions.len();
-            for stmt in else_block {
-                instructions.extend(compile(stmt));
-            }
-            
-            let end_pos = instructions.len();
-            
-            
-            if let Instruction::Jz(_) = instructions[jz_pos] {
-                instructions[jz_pos] = Instruction::Jz(else_block_start);
-            }
-            if let Instruction::Jmp(_) = instructions[jmp_pos] {
-                instructions[jmp_pos] = Instruction::Jmp(end_pos);
-            }
-            
-            instructions.push(Instruction::Label("if_end".to_string()));
+        ASTNode::If {
+            condition,
+            if_block,
+            else_block,
+        } => {
+            let mut instructions = compile(*condition);
+            let jz_target = instructions.len() + if_block.len() + 1;
+            instructions.push(Instruction::Jz(jz_target));
+            instructions.extend(if_block.into_iter().flat_map(compile));
+            let jmp_target = instructions.len() + else_block.len() + 1;
+            instructions.push(Instruction::Jmp(jmp_target));
+            instructions.extend(else_block.into_iter().flat_map(compile));
             instructions
         }
-        
         ASTNode::While { condition, body } => {
-            let mut instructions = Vec::new();
-            let mut real_pos = 0;
-            
-            let loop_start = real_pos;
-            instructions.push(Instruction::Label("while_start".to_string()));
-            
-            
-            let condition_instructions = compile(*condition);
-            real_pos += condition_instructions.len();
-            instructions.extend(condition_instructions);
-            
-            
-            let jz_pos = instructions.len();
-            instructions.push(Instruction::Jz(0));  
-            real_pos += 1;
-            
-            
-            for stmt in body {
-                let stmt_instructions = compile(stmt);
-                real_pos += stmt_instructions.len();
-                instructions.extend(stmt_instructions);
-            }
-            
-            
-            instructions.push(Instruction::Jmp(loop_start));
-            real_pos += 1;
-            
-            
-            if let Instruction::Jz(_) = instructions[jz_pos] {
-                instructions[jz_pos] = Instruction::Jz(real_pos);
-            }
-            
-            instructions.push(Instruction::Label("while_end".to_string()));
+            let mut instructions = compile(*condition);
+            let jz_target = instructions.len() + &body.len() + 2;
+            instructions.push(Instruction::Jz(jz_target));
+            instructions.extend(body.clone().into_iter().flat_map(compile));
+            let jmp_target = instructions.len() - (&body.len() + 1);
+            instructions.push(Instruction::Jmp(jmp_target));
             instructions
         }
-
-
     }
 }
 
 fn main() {
-    let input = "while (3 > 2) { 1 } ".to_string();
+    let input = "if (3 > 2) { 1 } else { 0 }".to_string();
 
-    
     let tokenizer = Tokenizer::new(input);
     let mut parser = Parser::new(tokenizer);
     let ast_nodes = parser.parse_program();
     println!("AST: {:?}", ast_nodes);
 
-    
     let mut vm = VM::new();
     for ast in ast_nodes {
         let instructions = compile(ast);
-        println!("Instructions: {:?}", instructions);
         vm.execute(&instructions);
     }
 
